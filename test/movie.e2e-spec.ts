80ca12f4e24f6c0e883f2da0f6fe412ca819042e
Advait Maindalkar <advait@incubyte.co>
2025-07-01 09:58:06 +0530

    test: added failing e2e test for movies

---
+++ test/movie.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { beforeAll, describe } from 'vitest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { MovieModule } from '../src/movie/movie.module';
+import * as request from 'supertest';
+
+describe('Movie E2E Suite:', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [MovieModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('should give the oldness of a movie, given the name:', async () => {
+    const movieName = 'Harry Potter and the Prisoner of Azkaban';
+    const response = await request(app.getHttpServer()).get(
+      `/movies/${movieName}/oldness`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
